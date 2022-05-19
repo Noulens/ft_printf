c3cb5574b3a38000378193abf3b2b494a8cb1bb8
Waxaklajuun <71551693+Noulens@users.noreply.github.com>
2022-05-19 18:27:51 +0200

    Update ft_lstnew_bonus.c

--- libft/ft_lstnew_bonus.c
+++ libft/ft_lstnew_bonus.c
@@ -23,23 +23,3 @@ t_list	*ft_lstnew(void *content)
 	first->next = NULL;
 	return (first);
 }
-
-/*#include <stdio.h>
-#include <stdlib.h>
-
-typedef struct s_list
-{
-	void			*content;
-	struct s_list	*next;
-} t_list;
-
-
-int	main(void)
-{
-	char	s[] = "ceci";
-	t_list	*ls;
-
-	ls = ft_lstnew((char *)s);
-	printf("%s", (char *)ls->content);
-	return (0);
-}*/
