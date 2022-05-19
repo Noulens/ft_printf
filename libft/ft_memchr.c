a7a7e617ae0f092b00afaf1c77c9b97f044fa42b
Waxaklajuun <71551693+Noulens@users.noreply.github.com>
2022-05-19 18:28:22 +0200

    Update ft_memchr.c

--- libft/ft_memchr.c
+++ libft/ft_memchr.c
@@ -28,15 +28,3 @@ void	*ft_memchr(const void *s, int c, size_t n)
 	}
 	return (NULL);
 }
-
-/*#include <stdio.h>
-#include <stdlib.h>
-
-int main()
-{
-	int c = 258;
-	int *p;
-	int	str[] = {1, 2 , 258, 4, 5};
-	p = ft_memchr(str, c, 3);
-    return p[0];
-}*/
