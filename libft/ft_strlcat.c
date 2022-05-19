1d951b0824aeb084b151f045ac883a41b8150dfa
Waxaklajuun <71551693+Noulens@users.noreply.github.com>
2022-05-19 18:32:18 +0200

    Update ft_strlcat.c

--- libft/ft_strlcat.c
+++ libft/ft_strlcat.c
@@ -40,31 +40,3 @@ size_t	ft_strlcat(char *dst, const char *src, size_t size)
 	*des = '\0';
 	return (dest_len + (sc - src));
 }
-
-/*#include <stdio.h>
-#include <bsd/string.h>
-#include <stdlib.h>
-
-size_t	ft_strlen(const char *s)
-{
-	int	i;
-
-	i = 0;
-	while (*(s + i))
-		++i;
-	return (i);
-}
-
-int	main(void)
-{
-char dest1[100] = "1234";
-char src1[100] = "ABCD";
-char dest2[100] = "1234";
-char src2[100] = "ABCD";
-
-	printf("sa fonction : %lu\n", ft_strlcat(dest1, src1, 3));
-	printf("la vrai fonction : %lu\n", strlcat(dest2, src2, 3));
-	printf("sa fonction : %s\n", dest1);
-	printf("la vrai fonction : %s\n", dest2);
-	return (0);
-}*/
