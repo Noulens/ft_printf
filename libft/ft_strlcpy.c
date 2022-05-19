b07d0cc9489e879f49ddf95b8c5e9b40535162d1
Waxaklajuun <71551693+Noulens@users.noreply.github.com>
2022-05-19 18:32:33 +0200

    Update ft_strlcpy.c

--- libft/ft_strlcpy.c
+++ libft/ft_strlcpy.c
@@ -33,16 +33,3 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 	}
 	return (i);
 }
-
-/*#include <stdio.h>
-#include <stdlib.h>
-
-int main()
-{
-int c;
-char dest[23];
-
-c = ft_strlcpy(dest, "ceci est un string0", 5);
-printf("Copied '%s' into '%s', size of src:%d", "ceci est un string0", dest, c);
-return (0);
-}*/
