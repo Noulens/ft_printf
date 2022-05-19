1ee564a81fcf96d2ae6b1a79922102c1f11aaa7a
Waxaklajuun <71551693+Noulens@users.noreply.github.com>
2022-05-19 18:29:40 +0200

    Update ft_putchar_fd.c

--- libft/ft_putchar_fd.c
+++ libft/ft_putchar_fd.c
@@ -16,15 +16,3 @@ void	ft_putchar_fd(char c, int fd)
 {
 	write(fd, &c, sizeof(c));
 }
-
-/*
-#include <unistd.h>
-
-int	main(void)
-{
-	char	c = ';';
-	int		fd = 1;
-
-	ft_putchar_fd(c, fd);
-	return (0);
-}*/
